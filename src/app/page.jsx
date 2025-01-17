7efe7f29f1832c0a0cd7accc531822efb0018d16
Muxtar Bayramov <muxtarbayramov@Muxtars-MacBook-Pro.local>
2025-01-17 09:45:03 +0400

    version2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,26 @@
+'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
+import { use, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight, faCopy } from '@fortawesome/free-solid-svg-icons'
 import '../../styles/css/main.css'
 
 export default function Home() {
-  return (
-    <div className="main">
-    	<div className="password-generator">
+	const [length, setLength] = useState(0);
+
+	const [upper, setUpper] = useState(false);
+	const [lower, setLower] = useState(false);
+	const [number, setNumber] = useState(false);
+	const [symbol, setSymbol] = useState(false);
+
+	function change(c){
+		setLength(c.target.value)
+	}
+
+	return (
+	<div className="main">
+		<div className="password-generator">
 			<div className="head">
 				<h3>Password generator</h3>
 			</div>
@@ -18,27 +31,35 @@ export default function Home() {
 			<div className="body">
 				<div className="head">
 					<p>Character length</p>
-					<p className="length">0</p>
+					<p className="length">{length}</p>
 				</div>
 				<div className="middle">
 					<div className="range">
-						<input type="range" name="" id="" max='20' min='1' defaultValue='0'/>
+						<input type="range" name="" id="" max='20' min='1' defaultValue='0' onChange={change}/>
 					</div>
 					<div>
-						<input type="checkbox" />
-						<label htmlFor="">Include Upparcase Letters</label>
+						<input type="checkbox" id="upper" onChange={(c)=>{
+							setUpper(c.target.checked)
+						}}/>
+						<label htmlFor="upper">Include Upparcase Letters</label>
 					</div>
 					<div>
-						<input type="checkbox" />
-						<label htmlFor="">Include Lowercase Letters</label>
+						<input type="checkbox" id="lower" onChange={(c)=>{
+							setLower(c.target.checked);
+						}}/>
+						<label htmlFor="lower">Include Lowercase Letters</label>
 					</div>
 					<div>
-						<input type="checkbox" />
-						<label htmlFor="">Include Numbers</label>
+						<input type="checkbox" id="number" onChange={(c)=>{
+							setNumber(c.target.checked);
+						}}/>
+						<label htmlFor="number">Include Numbers</label>
 					</div>
 					<div>
-						<input type="checkbox" />
-						<label htmlFor="">Include Symbols</label>
+						<input type="checkbox" id="symbol" onChange={(c)=>{
+							setSymbol(c.target.checked)
+						}}/>
+						<label htmlFor="symbol">Include Symbols</label>
 					</div>
 				</div>
 				<div className="strong">
@@ -51,13 +72,16 @@ export default function Home() {
 					</div>
 				</div>
 				<div className="submit">
-					<button>
+					<button onClick={()=>{
+						console.log('teleb olunanlar ===>', length, upper, lower, number, symbol);
+						
+					}}>
 						<p>Generate</p>
 						<FontAwesomeIcon icon={faArrowRight} />
 					</button>
 				</div>
 			</div>
 		</div>
-    </div>
-  );
+	</div>
+	);
 }
